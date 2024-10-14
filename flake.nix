af081c6464f4c2deb758ff21cc8d344b13d6bf84
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2024-10-14 04:36:27 -0300

    feat: nix flake for pushing code into esp32cam

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  description = "Flake for ESP32CamVisionAI with flake-utils for cross-platform support";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils"; # Add flake-utils for simplifying platform management
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShells = {
+            # Development shell for ESP32
+            esp = pkgs.mkShell {
+              name = "ESP Dev Environment";
+
+              # Common build inputs for all platforms
+              buildInputs =
+                with pkgs;
+                [
+                  rustc
+                  neovim
+                  clang
+                  libiconv
+                  espup
+                  ldproxy
+                  espflash
+                  just
+                ]
+                ++ (
+                  if pkgs.stdenv.isDarwin then
+                    [
+                      pkgs.darwin.apple_sdk.frameworks.SystemConfiguration # macOS-specific package
+                    ]
+                  else
+                    [ ]
+                );
+
+              # Commands to run when the shell is activated
+              shellHook = ''
+                # ESP setup
+                espup install
+                . $HOME/export-esp.sh
+
+                # Copy the config file
+                cp ./embedded/cfg.toml.example ./embedded/cfg.toml
+
+                echo "Development environment ready!"
+                echo "Please update file ./embedded/cfg.toml with your wifi credentials"
+                echo "After that, just run:"
+                echo "just esp32cam"
+              '';
+            };
+
+            # Additional shell could be defined here, like another devShell (e.g., "tools" or "build")
+            # Example of a shell for building or testing purposes
+            # tools = pkgs.mkShell {
+            #   name = "Tools Dev Environment";
+            #
+            #   buildInputs = with pkgs; [
+            #     rustc
+            #     cargo
+            #     clang
+            #     cmake
+            #     ninja
+            #     git
+            #   ];
+            #
+            #   shellHook = ''
+            #     echo "Tools development environment ready!";
+            #     # Here you can define any setup specific to this environment
+            #   '';
+            # };
+          };
+        }
+      );
+}
